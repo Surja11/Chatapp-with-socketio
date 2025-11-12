4bcbf71e3d3de3311888e53a4625eac1f3d0f6b1
Surja11 <stha.surja123@gmail.com>
2025-11-12 18:47:11 +0545

    corrected conflicts

--- client/server/database/models/user.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, EmailStr
-
-class User(BaseModel):
-  username : str
-  password : str
-  profile : str|None= None
-  email : EmailStr
--- client/server/schemas/chat.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class SendMessage(BaseModel):
-  user_id: str
-  friend_id : str
-  text : str
